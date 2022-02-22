52fd18d4117f5197508e9e7c60c9a018b3dd5a23
Astolfen <daniil.zarubin.03@mail.ru>
2022-02-22 21:08:30 +0300

    add set segment tree

--- main.cpp
+++ main.cpp
@@ -6,14 +6,14 @@
 typedef std::vector<int> IntVector;
 
 
-void build_segment_tree_(IntVector &segment, IntVector &segment_tree, int iWrite, int sl, int sr) {
+void build_segment_tree_(IntVector &segment, IntVector &segment_tree, int w, int sl, int sr) {
     if (sl == sr)
-        segment_tree[iWrite] = segment[sl];
+        segment_tree[w] = segment[sl];
     else {
         int sm = sl + (sr - sl) / 2;
-        build_segment_tree_(segment, segment_tree, iWrite * 2, sl, sm);
-        build_segment_tree_(segment, segment_tree, iWrite * 2 + 1, sm + 1, sr);
-        segment_tree[iWrite] = segment_tree[iWrite * 2] + segment_tree[iWrite * 2 + 1];
+        build_segment_tree_(segment, segment_tree, w * 2, sl, sm);
+        build_segment_tree_(segment, segment_tree, w * 2 + 1, sm + 1, sr);
+        segment_tree[w] = segment_tree[w * 2] + segment_tree[w * 2 + 1];
     }
 }
 
@@ -24,10 +24,23 @@ void build_segment_tree(IntVector &segment, IntVector &segment_tree) {
 //void sum_segment_tree(){
 //
 //}
-//
-//void set_segment_tree(){
-//
-//}
+
+void set_segment_tree_(IntVector &segment_tree, int w, int sl, int sr, int index, int x) {
+    if (sl == sr)
+        segment_tree[w] = x;
+    else {
+        int sm = sl + (sr - sl) / 2;
+        if (index <= sm)
+            set_segment_tree_(segment_tree, w * 2, sl, sm, index, x);
+        else
+            set_segment_tree_(segment_tree, w * 2 + 1, sm + 1, sr, index, x);
+        segment_tree[w] = segment_tree[w * 2] + segment_tree[w * 2 + 1];
+    }
+}
+
+void set_segment_tree(IntVector &segment_tree, int index, int x) {
+    set_segment_tree_(segment_tree, 1, 0, MAX_SIZE_SEGMENT - 1, index, x);
+}
 
 int main() {
     std::vector<int> segment(MAX_SIZE_SEGMENT);
@@ -38,6 +51,13 @@ int main() {
         std::cin >> x;
 
     build_segment_tree(segment, segment_tree);
+
+    for (auto &x: segment_tree)
+        std::cout << x << ' ';
+
+    set_segment_tree(segment_tree, 1, 2);
+
+    std::cout << std::endl;
 
     for (auto &x: segment_tree)
         std::cout << x << ' ';
